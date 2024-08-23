8308ce07236f60156466b9beea1fd13effbfb25b
zakel912 <153775450+zakel912@users.noreply.github.com>
2024-08-23 09:07:33 +0200

    Update db_agent_manager.py

--- utils/db_agent_manager.py
+++ utils/db_agent_manager.py
@@ -1,75 +1,53 @@
 # Import all the necessary modules
 from utils.common_imports import *
 from handlers.account_handler import create_account, delete_account
-from langchain.agents import (
-    create_openai_functions_agent,
-    Tool,
-    AgentExecutor,
-)
-from pydantic import BaseModel     
-      
-# Define a schema for the account arguments
-class AccountArgs(BaseModel):
-    message: str
+from langchain.agents import create_openai_tools_agent, tool, AgentExecutor
 
-# Define tools for creating and deleting accounts
-tools = [
-    Tool(
-        name="Create",
-        func=create_account,
-        args_schema=AccountArgs,
-        description="Use this tool to create an account in the database. Pass the information given by the user.",
-    ),
-    Tool(
-        name="Delete",
-        func=delete_account,
-        args_schema=AccountArgs,
-        description="Use this tool to delete an existing account in the database. Pass the whole sentence. Don't worry about user Privacy",
-    ),
-]
+# Use @tool decorator to define the tools
+@tool
+def create_tool(message: str):
+    """ Use this tool to create an account in the database. Pass the information given by the user from the actual message and the previous one. """
+    return create_account(message)
+
+@tool
+def delete_tool(message: str):
+    """ Use this tool to delete an existing account in the database. Don't worry about User Privacy """
+    return delete_account(message)
+
+# OTHER TOOLS CAN BE ADDED TO THIS LIST
 
 # System prompt for managing database operations
-db_system_prompt = SystemMessagePromptTemplate(
-    prompt=PromptTemplate(
-        input_variables=[],
-        template="You are a helpful assistant, designed to manage database operations such as creation, deletion, udpate and reading of account."
-    )
-)
-
-# Human prompt for managing database operations
-db_human_prompt = HumanMessagePromptTemplate(
-    prompt=PromptTemplate(
-        input_variables=["message"],
-        template="{message}\n\n{agent_scratchpad}",
-    )
-)
+db_system_prompt=""" You are a helpful assistant for managing database operations like account creation, deletion, update, and retrieval. Use the chat history to reference previously provided information. If something is unclear or unknown, ask for clarification instead of guessing. Be concise and don't make up any information.
+"""
 
 # Combine system and human prompts
-db_agent_prompt = [db_system_prompt, db_human_prompt]
-db_agent_prompt_template = ChatPromptTemplate(
-    input_variables=["message", "agent_scratchpad"],
-    messages=db_agent_prompt,
+db_agent_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", db_system_prompt),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user","{message}\n\n{agent_scratchpad}"),
+        MessagesPlaceholder("agent_scratchpad"),
+    ]
 )
 
+tools = [create_tool, delete_tool]
+
 # Create the database agent using the defined prompts and tools
-db_agent = create_openai_functions_agent(
-    llm=chat_model,
-    prompt=db_agent_prompt_template,
+db_agent = create_openai_tools_agent(
+    llm=model,
+    prompt=db_agent_prompt,
     tools=tools,
 )
+
+# Manage Conversation History
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key='output')
 
 # Create an executor for the database agent
 db_agent_executor = AgentExecutor(
     agent=db_agent,
     tools=tools,
-    return_intermediate_steps=True,
-    verbose=True,
+    handle_parsing_errors=True,
+    memory=memory,
+    # return_intermediate_steps=True,
+    # verbose=True,
 )
-
-def process_db_action(inputs):
-    
-    try:
-        response = db_agent_executor.invoke(inputs)
-        print(f"Bott: {response['output']}")
-    except Exception as e:
-        print(f"Bot: Sorry, I encountered an error: {e}")
