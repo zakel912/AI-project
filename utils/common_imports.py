584753cc0855ce044dad11a0316bb895862657c3
zakel912 <153775450+zakel912@users.noreply.github.com>
2024-08-23 09:07:00 +0200

    Update common_imports.py

--- utils/common_imports.py
+++ utils/common_imports.py
@@ -7,11 +7,13 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     AIMessagePromptTemplate,
     ChatPromptTemplate,
+    MessagesPlaceholder
 )
 from langchain_core.output_parsers import StrOutputParser
+from langchain.memory import ConversationBufferMemory
 
 # Set the OpenAI API key
 openai.api_key = OPENAI_API_KEY
 
 # Initialize the ChatOpenAI model
-chat_model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=50,  openai_api_key=OPENAI_API_KEY)
+model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, max_tokens=50,  openai_api_key=OPENAI_API_KEY)
