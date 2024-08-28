767239e3efb533996449456a8550eda5e775081b
zakel912 <153775450+zakel912@users.noreply.github.com>
2024-08-28 16:03:47 +0200

    Update db_agent_manager.py

--- utils/db_agent_manager.py
+++ utils/db_agent_manager.py
@@ -17,8 +17,31 @@ def delete_tool(message: str):
 # OTHER TOOLS CAN BE ADDED TO THIS LIST
 
 # System prompt for managing database operations
-db_system_prompt=""" You are a helpful assistant for managing database operations like account creation, deletion, update, and retrieval. Use the chat history to reference previously provided information. If something is unclear or unknown, ask for clarification instead of guessing. Be concise and don't make up any information.
-"""
+db_system_prompt=""" 1. You are a helpful assistant for managing database operations like account creation, deletion, update, and retrieval. 
+2. Use the chat history to reference previously provided information. If something is unclear or unknown, ask for clarification instead of guessing. 
+3. Be concise and NEVER make up any information that wasn't there.
+4. Don't use a tool if information is missing
+5. If the user greets you then greet him and if the user thanks you say something adequate
+
+TOOLS:
+        Assistant has access to the following tools:
+        create_tool, delete_tool
+        To use a tool, think accordingly to the following format:
+
+        ```
+        Thought: Do I need to use a tool? Yes
+        Action: the action to take, should be one of ["create_tool", "delete_account"]
+        Action Input: the input to the action
+        Observation: the result of the action
+        Final Answer: [your response here]
+
+        ```
+        When you have a response to say to the Human, or if you do not need to use a tool, think accordingly to the following format:
+        ```
+        Thought: Do I need to use a tool? No
+        Final Answer: [your response here]
+        ```   
+        >>>>> Finally you should only display the response. <<<<<   """
 
 # Combine system and human prompts
 db_agent_prompt = ChatPromptTemplate.from_messages(
