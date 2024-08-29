3592f7175878efffc69e1527946badd69a4c9601
zakel912 <153775450+zakel912@users.noreply.github.com>
2024-08-29 15:16:25 +0200

    Update account_handler.py

--- handlers/account_handler.py
+++ handlers/account_handler.py
@@ -1,19 +1,23 @@
 from database.user_handler import insert_user, find_user_by_name, update_user, delete_user
-from utils.user_info_parser import extract_user_info, check_missing_info
+from utils.user_info_parser import extract_user_info, check_missing_info, extract_fields
 
 # Create a new account
-def create_account(user_message):
-    user_info = extract_user_info(user_message)
 
-    if not user_info:
-        return "Sorry, I couldn't understand the information provided. Please try again."
-    if (check_missing_info(user_info)):
-        missing_fields_formatted = ', '.join(check_missing_info(user_info))
-        return (f"Bot: I still need the following information to create your account: {missing_fields_formatted}. Please provide it.")
-    
+def create_account(user_info):
     user_id = insert_user(user_info)
     return f"Account created for {user_info['first_name']} {user_info['last_name']}, age {user_info['age']}. User ID: {user_id}"
 
+def create_account_tool(user_message):
+    print(f"user message : {user_message}")
+    user_info = extract_fields(user_message)
+    print(f"user info : {user_info}")
+    if not user_info:
+        return "Sorry, I couldn't understand the information provided. Please try again."
+    if (check_missing_info(user_info)):
+        missing_fields_formatted = '\n- '.join(check_missing_info(user_info))
+        print (f"Bot: I still need the following information to create your account:\n- {missing_fields_formatted}.\n Please provide it.")
+    create_account(user_info)
+    
 # Delete User Account
 def delete_account(user_message):
     user_info = extract_user_info(user_message)
