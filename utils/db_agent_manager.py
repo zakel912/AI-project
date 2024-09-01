a70362a6a4ac952047cc6de8e3b7c10b77660ea3
zakel912 <153775450+zakel912@users.noreply.github.com>
2024-09-01 21:09:45 +0200

    Update db_agent_manager.py

--- utils/db_agent_manager.py
+++ utils/db_agent_manager.py
@@ -1,65 +1,66 @@
 # Import all the necessary modules
 from utils.common_imports import *
-from handlers.account_handler import create_account_tool, delete_account
+from handlers.account_handler import create_account_tool, delete_account_tool, update_account_tool
 from langchain.agents import create_openai_tools_agent, tool, AgentExecutor
 
 # Use @tool decorator to define the tools
 @tool
 def create_tool(message: str):
-    """ Use this tool if the user provides you with all the required information and wishes to create an account. 
+    """Use this tool if the user provides you with all the required information and wishes to create an account. 
     Be brief.
-    No need to specify "missing" or "unknown" .
-    There are six essential and required information for creating an account:
+    Writes in the message you invoke only from the information you just have been given.
+    No need to specify "missing" or "unknown" 
+    There are six essential pieces of information required to creating an account:
     - First name
     - Last name
     - Age
     - Gender (male or female)
     - Email
     - Password
-    If one essential information for the creation of an account is missing, please ask for it .
+    If one essential information for the creation of an account is missing, please ask for it. 
 """ 
     return create_account_tool(message)
 
 @tool
 def delete_tool(message: str):
-    """ Use this tool to delete an existing account in the database. Don't worry about User Privacy.
-    Writes in the message you invoke only the information you have.
-    No need to specify "missing" or "unknown" 
-    there are two essential and required information for deleting an account:
+    """ Use this tool if the user provides you with all the required information and wishes to delete an account.
+    Don't worry about User Privacy.
+    There are three essential pieces of information required to delete an account:
     - First name
     - Last name
+    - Password
     If one essential information for the creation of an account is missing, please ask for it. 
     """
-    return delete_account(message)
+    return delete_account_tool(message)
+
+@tool
+def update_tool(message: str):
+    """ Use this tool to update information in user account. 
+    Instructions:
+    1. Focus solely on the information provided by the user. Do not add or create any information not explicitly given.
+    2. Don't worry about User Privacy.
+    3. There are three essential pieces of information required to update an account:
+
+   - Email
+   - Password
+   - The specific information to update.
+    """
+    return update_account_tool(message)
 
 # OTHER TOOLS CAN BE ADDED TO THIS LIST
-
-tools = [create_tool, delete_tool]
-tools_name = ["create_tool", "delete_account"]
+# List of tools
+tools = [create_tool, delete_tool, update_tool]
+# List of tools name
+tools_name = ["create_tool", "delete_account", "update_account"]
 
 # System prompt for managing database operations
 db_system_prompt=""" 
-1. You are a helpful assistant for managing database operations like account creation, deletion, update, and retrieval. 
-2. Use the chat history to reference previously provided information. 
-3. NEVER make up any information that wasn't there.
-4. USE a tool ONLY if you have all the required information
 
-TOOLS:
-        Assistant has access to the following tools:
-        create_tool, delete_tool
-        To use a tool, please use the following format:
+- You are a helpful assistant for managing database operations like account creation, deletion, update, and retrieval. 
+- Focus on the new message but use the chat history to reference previously provided information.  
+- Never make up any information that wasn't there. Ask for information if not given by the user
+- Think twice before using a tool, you need required information to use one.
 
-        ```
-        Thought: Do I need to use a tool? Yes
-        Action: the action to take, should be one of ["create_tool", "delete_account"]
-        Action Input: the input to the action
-        Observation: the result of the action
-        ```
-        When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-        ```
-        Thought: Do I need to use a tool? No
-        Final Answer: [your response here]
-        ```         
 """
 
 # Combine system and human prompts
@@ -72,7 +73,6 @@ db_agent_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-
 # Create the database agent using the defined prompts and tools
 db_agent = create_openai_tools_agent(
     llm=model,
@@ -83,12 +83,12 @@ db_agent = create_openai_tools_agent(
 # Manage Conversation History
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, output_key='output')
 
-# Create an executor for the database agent
+# Create an executor for the database agent that can be called up later 
 db_agent_executor = AgentExecutor(
     agent=db_agent,
     tools=tools,
     handle_parsing_errors=True,
     memory=memory,
-    return_intermediate_steps=True,
-    verbose=True,
+    # return_intermediate_steps=True,
+    # verbose=True,
 )
